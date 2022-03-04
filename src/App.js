c34a21c6e35d64cbf3342ead241ed4f34fddf588
Rana Sam <ranausama2082001@gmail.com>
2022-03-04 20:46:34 +0500

    ads

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Services from './services/Services';
 import Navbar from './navbar/Navbar';
 import Reasons from './reasons/Reasons';
 import States from './states/States';
+import Ads from './ads/Ads';
 
 function App() {
   return (
     <div >
+      <Ads/>
       <States/>
       <Services/>
       <AboutUS/>
