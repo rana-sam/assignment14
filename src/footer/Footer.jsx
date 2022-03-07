a52f5563dad2ef35d689c2ae26dfd73c20d9cc53
Rana Sam <ranausama2082001@gmail.com>
2022-03-07 22:52:42 +0500

    Update Footer.jsx

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div>
-        <div className="container mt-5">
+        <div className="container mt-3 mb-3">
             <div className="row">
                 <div className="col-12 bg-primary">
                     <p className='text-light ps-5 ms-5 pt-3'>TheBox Company © 2022. All Rights Reserved</p>
