30c29abb8306b643c84dc28455008f1e8728552e
Rana Sam <ranausama2082001@gmail.com>
2022-03-07 21:50:37 +0500

    bottom

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import Reasons from './reasons/Reasons';
 import States from './states/States';
 import Ads from './ads/Ads';
 import Cta from './cta/Cta';
+import Bottom from './bottom/Bottom';
 
 function App() {
   return (
     <div >
       
+      <Bottom/>
       <Cta/>
       <Ads/>
       <States/>
