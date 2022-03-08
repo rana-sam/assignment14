cb5ca5664fcb82034d56bfa26242b7daf3c04417
Rana Sam <ranausama2082001@gmail.com>
2022-03-08 21:09:05 +0500

    Update Footer.jsx

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
     <div>
-        <div className="container mt-3 mb-3">
-            <div className="row">
-                <div className="col-12 bg-primary">
-                    <p className='text-light ps-5 ms-5 pt-3'>TheBox Company © 2022. All Rights Reserved</p>
-                </div>
-            </div>
+      <div className="container mt-3 mb-3">
+        <div className="row">
+          <div className="col-12 bg-primary">
+            <p className="text-light ps-5 ms-5 pt-3">
+              TheBox Company © 2022. All Rights Reserved
+            </p>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
